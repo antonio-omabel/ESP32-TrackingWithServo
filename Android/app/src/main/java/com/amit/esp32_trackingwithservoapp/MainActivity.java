da5f2e5cb2376ee6bfd60c2f1266f7910283aeee
castagnoemanuele <castagnoemanuele3@gmail.com>
2023-07-20 18:35:15 +0200

    added cameraX preview

--- Android/app/src/main/java/com/amit/esp32_trackingwithservoapp/MainActivity.java
+++ Android/app/src/main/java/com/amit/esp32_trackingwithservoapp/MainActivity.java
@@ -1,6 +1,13 @@
 package com.amit.esp32_trackingwithservoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,10 +18,13 @@ import android.widget.TextView;
 
 import com.amit.esp32_trackingwithservoapp.Interfaces.IMyRotationVector;
 import com.amit.esp32_trackingwithservoapp.Sensor.MyRotationVector;
+import com.google.common.util.concurrent.ListenableFuture;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -24,6 +34,8 @@ import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity implements IMyRotationVector {
     private String TAG = "MainActivity";
+
+    private PreviewView previewView;
     private Button bttRotateClockwise=null, bttRotateCounterclockwise=null,bttStart = null,
             bttStop = null, bttGO=null, bttSlowSpeed = null,bttNormalSpeed = null, bttFastSpeed=null;
     private TextView tvHorizontalValue = null;
@@ -32,6 +44,10 @@ public class MainActivity extends AppCompatActivity implements IMyRotationVector
  
     /*private MyAccelerometer myAccelerometer = null;*/
     private MyRotationVector myRotationVector = null;
+
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+    private ImageCapture imageCapture;
+
     public MainActivity() throws MalformedURLException, UnsupportedEncodingException {
     }
 
@@ -79,6 +95,21 @@ public class MainActivity extends AppCompatActivity implements IMyRotationVector
     }
 
     private void Init(){
+        //CameraX initialization
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                startCameraX(cameraProvider);
+            } catch (ExecutionException e){
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        },getExecutor());
+
+
+
         bttRotateClockwise=findViewById(R.id.bttRotateClockwise);
         bttRotateCounterclockwise=findViewById(R.id.bttRotateCounterclockwise);
         bttStart = findViewById(R.id.bttStart);
@@ -88,6 +119,8 @@ public class MainActivity extends AppCompatActivity implements IMyRotationVector
 
         myRotationVector = new MyRotationVector(this, this);
         tvHorizontalValue = findViewById(R.id.tvHorizontalValue);
+
+        previewView = findViewById(R.id.view_finder);
 
         etIP =  findViewById(R.id.etIP);
         etIPpart1 =  findViewById(R.id.etIPPart1);
@@ -102,7 +135,29 @@ public class MainActivity extends AppCompatActivity implements IMyRotationVector
         bttNormalSpeed = findViewById(R.id.bttNormalSpeed);
         bttFastSpeed = findViewById(R.id.bttFastSpeed);
     }
-    
+
+    private Executor getExecutor() {
+        return ContextCompat.getMainExecutor(this);
+    }
+
+    private void startCameraX(ProcessCameraProvider cameraProvider) {
+        cameraProvider.unbindAll();
+        //Cameraselector use case
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+        //Preview Use Case
+        Preview preview = new Preview.Builder().build();
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+        // Image capture use case
+        imageCapture = new ImageCapture.Builder()
+                .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+                        .build();
+        
+        cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector,preview, imageCapture);
+
+    }
+
     private void httpRequest(String data){
         //TODO: fix error "http request fail when rotation works (happens with 900+° degrees rotation)
         //TODO: move in separate class
