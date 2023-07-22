597dbc4a175ef43ab6af625a7978127b9e08a9bf
Antonio Omabel Longhitano <antonioomabel.longhitano@gmail.com>
2023-07-22 14:58:08 +0200

    Activities corrected

--- Android/app/src/main/java/com/amit/esp32_trackingwithservoapp/MainActivity.java
+++ Android/app/src/main/java/com/amit/esp32_trackingwithservoapp/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        setContentView(R.layout.activity_main);
         Log.i(TAG, "onCreate");
         Init();
 
